aff274a17e94844a19b52ca4fb7818a56733a10a
Dean Strelau <dean@strelau.net>
2024-11-06 11:04:05 -0600

    try again

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,6 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
-    paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
-    },
     adapter: adapter({
       pages: "build",
       assets: "build",
