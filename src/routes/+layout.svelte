3f3e19824ac474432528bf2bec00d4ce96d51fcc
Dean Strelau <dean@strelau.net>
2024-11-07 21:12:56 -0600

    add different collations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,61 @@
 <script lang="ts">
     import "@picocss/pico";
     import { page } from "$app/stores";
+    import { type Family, type Monster } from "$lib/Bestiary.svelte.js";
 
     let { data, children } = $props();
-    function slugify(s: string) {
-        return s
-            .toLowerCase()
-            .replace(/\s+/g, "-")
-            .replace(/[^\w-]+/g, "");
+    let collate = $state("By Name");
+    function build() {
+        function allMonstersBy(f: (m: Monster) => string): {
+            [key: string]: Monster[];
+        } {
+            let allMonsters = data.families.flatMap((f: Family) => f.monsters);
+            let monstersBy: { [key: string]: Monster[] } = {};
+            allMonsters.forEach((m: Monster) => {
+                const key = f(m);
+                if (!monstersBy[key]) {
+                    monstersBy[key] = [];
+                }
+                monstersBy[key].push(m);
+            });
+            return monstersBy;
+        }
+        switch (collate) {
+            case "By Name":
+                let allByFirstLetter = allMonstersBy((m: Monster) =>
+                    m.name[0].toUpperCase(),
+                );
+                return Object.entries(allByFirstLetter)
+                    .sort(([a], [b]) => a > b)
+                    .map(([letter, monsters]) => ({
+                        header: letter,
+                        monsters: monsters,
+                    }));
+            case "By Family":
+                return data.families.map((family: Family) => ({
+                    header: family.name,
+                    monsters: family.monsters,
+                }));
+            case "By Level":
+                const allByLevel = allMonstersBy((m: Monster) => m.level);
+                return Object.entries(allByLevel)
+                    .sort(([a], [b]) => {
+                        const parseLevel = (lvl: string) => {
+                            if (lvl.includes("/")) {
+                                const [num, denom] = lvl.split("/");
+                                return Number(num) / Number(denom);
+                            }
+                            return Number(lvl);
+                        };
+                        return parseLevel(a) - parseLevel(b);
+                    })
+                    .map(([lvl, monsters]) => ({
+                        header: lvl,
+                        monsters: monsters,
+                    }));
+        }
     }
+    let toc = $derived.by(build);
 </script>
 
 <div class="wrap">
@@ -17,30 +64,33 @@
     </header>
     <div class="container">
         <aside>
+            <select name="collate" bind:value={collate}>
+                <option selected>By Name</option>
+                <option selected>By Family</option>
+                <option selected>By Level</option>
+            </select>
             <nav>
-                {#each data.families as family}
-                    <details>
-                        <summary>{family.name}</summary>
-                        <ul>
-                            {#each family.monsters as monster}
-                                <li>
-                                    <a
-                                        href="/{monster.slug}"
-                                        aria-current={$page.url.pathname ===
-                                            `/${monster.slug}`}
-                                    >
-                                        {monster.name}
-                                    </a>
-                                </li>
-                            {/each}
-                        </ul>
-                    </details>
+                {#each toc as { header, monsters }}
+                    <summary>{header}</summary>
+                    <ul>
+                        {#each monsters as monster}
+                            <li>
+                                <a
+                                    href="/{monster.slug}"
+                                    aria-current={$page.url.pathname ===
+                                        `/${monster.slug}`}
+                                >
+                                    {monster.name}
+                                </a>
+                            </li>
+                        {/each}
+                    </ul>
                 {/each}
-                <ul>
+                <!-- <ul>
                     <li>
                         <a href="/_dice">Dice Roller</a>
                     </li>
-                </ul>
+                </ul> -->
             </nav>
         </aside>
         <div class="detail">
@@ -52,8 +102,7 @@
 <style>
     .wrap {
         margin: 5px auto;
-        max-width: 900px;
-        display: block;
+        max-width: 1200px;
     }
     header {
         padding: 5px 25px;
@@ -65,14 +114,14 @@
         align-items: stretch;
     }
     .detail {
-        flex-grow: 2;
-        max-width: 720px;
+        flex-grow: 4;
+        margin: auto 15px;
+        max-width: 70%;
     }
 
-    nav {
+    aside {
         overflow-y: auto;
-        height: 100%;
-        min-width: 20%;
+        flex-grow: 1;
     }
 
     nav ul {
@@ -81,7 +130,7 @@
     }
     nav li {
         padding-bottom: 0;
-        font-size: 15px;
+        font-size: 20px;
         margin-left: 0;
     }
     nav summary {
