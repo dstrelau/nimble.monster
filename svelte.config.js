6fe849b90d7752d4c0f77999a0d801626959b5e3
Dean Strelau <dean@strelau.net>
2024-11-14 22:37:18 -0600

    some vercel shit maybe

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,8 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
+    adapter: adapter({}),
   },
 };
