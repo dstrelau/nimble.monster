f2bd2c574c5e80e2969569e99b80045057a65eaf
Dean Strelau <dean@strelau.net>
2025-05-20 11:03:14 -0500

    instr: disable noisy metrics for now

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,20 +1,16 @@
 import { resourceFromAttributes } from "@opentelemetry/resources";
-import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 import { HttpInstrumentation } from "@opentelemetry/instrumentation-http";
 
 const HONEYCOMB_API_KEY = process.env.HONEYCOMB_API_KEY ?? "";
-const HONEYCOMB_DATASET = process.env.HONEYCOMB_DATASET ?? "nimble.monster";
 
 const useHoneycomb = !!HONEYCOMB_API_KEY;
 const otelHost = useHoneycomb
   ? "https://api.honeycomb.io"
   : "http://localhost:4318";
 const traceEndpoint = `${otelHost}/v1/traces`;
-const metricEndpoint = `${otelHost}/v1/metrics`;
 
 console.log(
   `OpenTelemetry exporting to: ${useHoneycomb ? "Honeycomb.io" : "localhost:4318"}`,
@@ -25,27 +21,19 @@ const traceExporter = new OTLPTraceExporter({
   headers: { "x-honeycomb-team": HONEYCOMB_API_KEY },
 });
 
-const metricExporter = new OTLPMetricExporter({
-  url: metricEndpoint,
-  headers: {
-    "x-honeycomb-team": HONEYCOMB_API_KEY,
-    "x-honeycomb-dataset": HONEYCOMB_DATASET,
-  },
-});
-
 const httpInstrumentation = new HttpInstrumentation({
   requestHook: (span, request) => {
     // For IncomingMessage (server requests), headers is directly available
     // For ClientRequest (client requests), getHeader method is used
-    if ('headers' in request && request.headers && request.headers.host) {
-      span.setAttribute('http.request.header.host', request.headers.host);
-    } else if ('getHeader' in request) {
-      const host = request.getHeader('host');
+    if ("headers" in request && request.headers && request.headers.host) {
+      span.setAttribute("http.request.header.host", request.headers.host);
+    } else if ("getHeader" in request) {
+      const host = request.getHeader("host");
       if (host) {
-        span.setAttribute('http.request.header.host', host.toString());
+        span.setAttribute("http.request.header.host", host.toString());
       }
     }
-  }
+  },
 });
 
 const sdk = new NodeSDK({
@@ -53,9 +41,6 @@ const sdk = new NodeSDK({
     [ATTR_SERVICE_NAME]: "nimble.monster",
   }),
   traceExporter,
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: metricExporter,
-  }),
   instrumentations: [httpInstrumentation],
 });
 
