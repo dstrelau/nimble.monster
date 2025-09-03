5e19908b1b1d25ebb5e4513a42ff0ab83ee1e33a
Dean Strelau <dean@strelau.net>
2025-09-03 10:32:15 -0500

    fix: simpleicons dark mode

--- components/SimpleIcons.tsx
+++ components/SimpleIcons.tsx
@@ -1,15 +1,16 @@
 import { siDiscord, siGithub } from "simple-icons";
 import { cn } from "@/lib/utils";
 
+const defaultClassName = "size-4 stroke-foreground fill-foreground";
 export const Discord = ({ className }: { className?: string }) => (
-  <svg viewBox="0 0 24 24" className={cn("size-4", className)}>
+  <svg viewBox="0 0 24 24" className={cn(defaultClassName, className)}>
     <title>{siDiscord.title}</title>
     <path d={siDiscord.path} />
   </svg>
 );
 
 export const GitHub = ({ className }: { className?: string }) => (
-  <svg viewBox="0 0 24 24" className={cn("size-4", className)}>
+  <svg viewBox="0 0 24 24" className={cn(defaultClassName, className)}>
     <title>{siGithub.title}</title>
     <path d={siGithub.path} />
   </svg>
