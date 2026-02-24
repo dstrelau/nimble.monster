84d3a3f9760d34f21513ace92d77caba0dd97d17
Dean Strelau <dean@strelau.net>
2026-02-23 21:46:34 -0600

    fix: migration

--- migrations/0006_naive_satana.sql
+++ migrations/0006_naive_satana.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `class_ability_lists` ADD `source_id` text REFERENCES sources(id);--> statement-breakpoint
-ALTER TABLE `spells` ADD `utility` integer DEFAULT false NOT NULL;
+ALTER TABLE `class_ability_lists` ADD `source_id` text REFERENCES sources(id);
